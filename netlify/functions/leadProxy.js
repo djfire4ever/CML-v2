4850c8da7a122356e2256bda837d898374d444bd
djfire4ever <djfire4ever@gmail.com>
2025-10-24 18:31:46 -0400

    updateleadProxy.js

--- netlify/functions/leadProxy.js
+++ netlify/functions/leadProxy.js
@@ -1,5 +1,5 @@
 export async function handler(event) {
-    const url = "https://script.google.com/macros/s/AKfycbz0n1Br3EO0z7Dukhqo0bZ_QKCZ-3hLjjsLdZye6kBPdu7Wdl7ag9dTBbgiJ5ArrCQx/exec";
+    const url = "https://script.google.com/macros/s/AKfycbz0n1Br3EO0z7Dukhqo0bZ_QKCZ-3hLjjsLdZye6kBPdu7Wdl7ag9dTBbgiJ5ArrCQ/exec";
   
     try {
       const isGet = event.httpMethod === "GET";
